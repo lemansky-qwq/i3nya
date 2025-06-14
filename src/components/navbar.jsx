174322cb6904b4a4d398b7283c0279a337994cc1
lemansky-qwq <236016695@qq.com>
2025-06-14 18:31:03 +0800

    合并联系页面

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -5,7 +5,6 @@ export default function Navbar() {
     <nav className="navbar">
       <Link to="/">首页</Link>
       <Link to="/about">关于</Link>
-      <Link to="/contact">联系</Link>
       <Link to="/games">小游戏</Link>
     </nav>
   );
