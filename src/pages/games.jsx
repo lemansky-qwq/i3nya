ff3bb8bb5761d2a7745c8653b4d7cca6ec6dacba
lemansky-qwq <236016695@qq.com>
2025-06-15 16:28:28 +0800

    1.4.2

--- src/pages/games.jsx
+++ src/pages/games.jsx
@@ -5,10 +5,12 @@ export default function Games() {
     <div>
       <h1>小游戏</h1>
       <ul>
-        <li><Link to="/games/click">🖱️点击</Link></li>
-        <p>全站首发，致敬老牌魔王：点击</p>
+        <li><Link to="/games/click">点击</Link></li>
+        <p>全站首发，致敬老牌魔王</p>
         <li><Link to="/games/jump">🕹️跳一跳</Link></li>
-        <p>跳一跳寸止版</p>
+        <p>Jump 1 Jump 3.3</p>
+        <li><Link to="/games/2048">2048</Link></li>
+        <p>2048 1.1</p>
       </ul>
     </div>
   );
