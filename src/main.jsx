d6d9f310d6cb9b94bfe07fa0454ca3e346d3cfd9
lemansky-qwq <236016695@qq.com>
2025-06-15 16:03:32 +0800

    1.4.0.2

--- src/main.jsx
+++ src/main.jsx
@@ -7,9 +7,7 @@ import './style.css';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-      <AuthProvider> {}
         <App />
-      </AuthProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
