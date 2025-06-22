b0cc9adfec4577f5b0ca7123caf9c2d297236509
lemansky-qwq <236016695@qq.com>
2025-06-22 15:58:04 +0800

    console.log

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import App from './App';
+import App from './app';
 import './style.css';
+
+console.log('ENV:', import.meta.env);
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
